e3042167003a23e396e2f3e0c3cecbf837f4241c
hhow09 <hhow09@gmail.com>
2025-12-29 21:48:11 +0100

    Handle invalid commands #cz2

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut command = String::new();
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Failed to read line");
+    println!("{}: command not found", command.trim());
 }
